ecc9f7458c303b579f87cb9f08b0976fa5d1bef1
Rodrigo Vazquez Asenjo <r.vazquezasenjo@gmail.com>
2025-01-03 17:58:21 +0000

    feat: Added the News Model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique = True, db_index=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+        related_name="news_articles"
+    )
+
+    def __str__(self):
+        return self.title
+        
+    class Meta:
+        verbose_name = "News Article"
+        verbose_name_plural = "News Articles"
