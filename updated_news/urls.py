15786d68208e0745c96068f35fc2c44b2852e696
Rodrigo Vazquez Asenjo <r.vazquezasenjo@gmail.com>
2025-01-05 09:44:14 +0000

    feat: - logins/logouts added to the base.html

--- updated_news/urls.py
+++ updated_news/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('news.urls')),
     path('summernote/', include('django_summernote.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
 
