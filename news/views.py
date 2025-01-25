c263c187fa2d51b4519419f8a7b2ec951ffab8f4
Rodrigo Vazquez Asenjo <r.vazquezasenjo@gmail.com>
2025-01-25 18:20:10 +0000

    fix: small changes to the views.py and login checks added

--- news/views.py
+++ news/views.py
@@ -1,34 +1,33 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views import generic
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import News, UserProfile, Comment, Likes, Organization
 from .forms import CommentForm
 
 
 def all_news(request):
     news = News.objects.all()
-
     template = 'news/news_list.html'
     context = {
         'page_title': 'Feed',
         'news': news
     }
-
-
     return render(request, template, context)
 
-
+@login_required
 def detail_news(request, slug):
     news_article = get_object_or_404(News, slug=slug)
     new_comment = None
 
     if request.method == 'POST':
         if 'like' in request.POST:
-            existing_like = news_article.likes.filter(user=request.user).first()
-            if existing_like:
-                existing_like.delete()
+            like = Likes.objects.filter(news_article=news_article, user=request.user).first()
+            if like:
+                like.delete()
             else:
-                Like.objects.create(user=request.user, news=news_article)
+                Likes.objects.create(user=request.user, news_article=news_article)
 
         if 'read_later' in request.POST:
             if request.user in news_article.read_later.all():
@@ -40,7 +39,7 @@ def detail_news(request, slug):
         if comment_form.is_valid():
             new_comment = comment_form.save(commit=False)
             new_comment.news = news_article
-            new_comment.author = request.user
+            new_comment.commenter = request.user
             new_comment.save()
 
         return redirect(news_article.get_absolute_url())
@@ -53,14 +52,13 @@ def detail_news(request, slug):
         'page_title': news_article.title,
         'news_article': news_article,
         'comment_form': comment_form,
-        'liked': news_article.likes.filter(user=request.user).exists(),
+        'liked': Likes.objects.filter(news_article=news_article, user=request.user).exists(),
         'read_later': request.user in news_article.read_later.all(),
     }
 
     return render(request, template, context)
 
-
-class ReadLater(generic.ListView):
+class ReadLater(LoginRequiredMixin, generic.ListView):
     model = News
     template_name = 'news/read_later.html'
     context_object_name = 'read_later'
